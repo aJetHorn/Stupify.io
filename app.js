5a529edb08a1d3f8790f15671edafeda63dd4b50
aJetHorn <tohearn94@aol.com>
2014-09-27 18:32:59 -0400

    Sample UI for class assignment - I would never make a UI this ugly

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 $(document).ready( function () 
 {
-	//...
+	var rawText;
+	var tokens;
+  $( "#stupifyButton" ).on( "click",   function() { //freely interact with displays
+  	rawText = $("#source").val().trim();
+  	tokens = rawText.split(" ");
+    highlightResultText();
+    console.log(rawText);
+    console.log(tokens);
+    postResultTokens();
+  });
+
+  function highlightResultText(){
+  	$("#result").css("background-color", "#ecf0f1");
+  	//$( this ).css( "width", "+=200" );
+  }
+
+  function postResultTokens(){
+  	var content = "";
+  	for (var i = 0; i < tokens.length; i++){
+  		content += tokens[i] + " ";
+  	}
+  	$("#result").val(content);
+  }
 })
